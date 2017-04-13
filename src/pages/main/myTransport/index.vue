a9946615884547a5eebe8def576a1eb04dca4d65
Alex-xd <i@alexxd.com>
2017-04-13 22:40:19 +0800

    refactor: 完成共享平台界面 和 仓储出租界面 UI

---
+++ src/pages/main/myTransport/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'myTransport',
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
