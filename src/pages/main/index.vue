339c8fb0e8dcba6c9d8e7a925e17a66be80ac8fa
Inconsolable523 <1273258990@qq.com>
2017-04-17 19:36:06 +0800

    docs:

--- src/pages/main/index.vue
+++ src/pages/main/index.vue
@@ -32,10 +32,6 @@
                             <li>
                                 <router-link to="/main/myTransport">账号设置</router-link>
                             </li>
-                            <li class="navbar-login">
-                                <router-link to="/login" class="btn btn-raised btn-info">登录</router-link>
-                                <router-link to="/login/register" class="btn btn-raised btn-default">注册</router-link>
-                            </li>
                         </ul>
                         <div class="slider shor slider-success"></div>
                     </div>
